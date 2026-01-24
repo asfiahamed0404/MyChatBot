d76f7a0d189ad99072a2e3d80b9f92cb143141f8
asfiahamed0404 <muasfiahamed276@gmail.com>
2026-01-24 23:10:57 +0530

    import PdfReader from PyPDF2 and extract page by page

--- MyChatBot.py
+++ MyChatBot.py
@@ -1,7 +1,16 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 
 st.header("NoteBot")
 
 with st.sidebar:
     st.title("My Notes")
     file = st.file_uploader("Upload notes PDF and start asking questions", type="pdf")
+
+#extracting the text from pdf file
+if file is not None:
+    my_pdf=PdfReader(file)
+    text=" "
+    for page in my_pdf.pages:
+        text += page.extract_text()
+        st.write(text)
